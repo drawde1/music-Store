b12c976a5f2c6174bb2da8408274889848d23edf
edward arneson-perkins <ejperkins514@gmail.com>
2021-06-21 13:16:58 -0400

    setup view contoller files set up story board files WIP designed product page

---
+++ Music Store/ProductViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ProductViewController.swift
+//  Music Store
+//
+//  Created by Rave Bizz on 6/21/21.
+//
+
+import UIKit
+
+class ProductViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
