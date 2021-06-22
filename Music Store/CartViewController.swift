a356201075ce9d6ec40ba784c01419bc731576d6
edward arneson-perkins <ejperkins514@gmail.com>
2021-06-22 14:14:05 -0400

    designed cart view

--- Music Store/CartViewController.swift
+++ Music Store/CartViewController.swift
@@ -9,21 +9,32 @@ import UIKit
 
 class CartViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        configureTable()
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func configureTable(){
+        let nib = UINib(nibName: "CartTableViewCell", bundle: nil)
+        tableView.register(nib, forCellReuseIdentifier: "CartCell")
+        tableView.dataSource = self
     }
-    */
 
+}
+extension CartViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 24
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "CartCell", for: indexPath) as? CartTableViewCell else{
+            print("table cell failed to convert")
+            return UITableViewCell()
+        }
+        return cell
+    }
+    
+    
 }
