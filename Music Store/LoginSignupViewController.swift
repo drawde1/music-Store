966e0556d329fe785b08fa67ca73b460aff00573
faridk <git@faridk.com>
2021-06-23 16:48:25 -0400

    auth

--- Music Store/LoginSignupViewController.swift
+++ Music Store/LoginSignupViewController.swift
@@ -10,34 +10,89 @@ import CoreData
 
 class LoginSignupViewController: UIViewController {
 
+    @IBOutlet weak var loginField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var signupButton: UIButton!
+
+    @IBAction func onSignup(_ sender: Any) {
+        if loginField.text != "" || passwordField.text != "" {
+            createUser(login: loginField.text ?? "user", password: passwordField.text ?? "123")
+            let alert = UIAlertController(title: "Success!", message: "Thanks for signing up! Please login now!", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+            self.present(alert, animated: true)
+        } else {
+           let alert = UIAlertController(title: "Error", message: "Login/Password cannot be empty", preferredStyle: .alert)
+           alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+           self.present(alert, animated: true)
+       }
+    }
+        
+    
+    @IBAction func onLogin(_ sender: Any) {
+//        deleteAllUsers()
+        if loginField.text != "" || passwordField.text != "" {
+            let fetchedUser = fetchUser(login: loginField.text!, password: passwordField.text!)
+            if  fetchedUser != nil {
+                UserModel.shared.user = fetchedUser!
+                // GOTO PROFILE
+                let storyBoard : UIStoryboard = UIStoryboard(name: "Profile", bundle:nil)
+                let profileViewController = storyBoard.instantiateViewController(withIdentifier: "ProfileView")
+                self.navigationController?.pushViewController(profileViewController, animated: true)
+            } else {
+                let alert = UIAlertController(title: "Error", message: "Login/Password are incorrect", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+                self.present(alert, animated: true)
+            }
+            
+        } else {
+            let alert = UIAlertController(title: "Error", message: "Login/Password cannot be empty", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+            self.present(alert, animated: true)
+        }
+        fetchData()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        createUser()
+//        createUser()
 //        fetchData()
 //        fetchOneData(with: "Mattt", and: "123")
         
         // Do any additional setup after loading the view....
     }
 
-    func createUser(){
+    func createUser(login: String, password: String){
         let appDelegate = UIApplication.shared.delegate as? AppDelegate
         let container = appDelegate?.persistentContainer
         guard let viewContext = container?.viewContext else {return}
         
         let user = User(context: viewContext)
         
-        user.password = "123"
-        user.name = "Matt"
-        UserModel.shared.user = user
-
-//        let song = Song(context: viewContext)
-//        song.name = "Fire"
-//        song.price = 10
-//        song.owner = user
-//
-//        print(song)
-//        print(user)
+        user.password = password
+        user.name = login
+        user.currentPoint = 0
+        user.pointTreward = 0
+        
+        let song = Song(context: viewContext)
+        song.name = "Fire"
+        song.price = 10
+        song.owner = user
+        
+        print(song)
+        print(user)
         try? viewContext.save()
+    }
+    
+    func deleteAllUsers() {
+        let appDelegate = UIApplication.shared.delegate as? AppDelegate
+        let container = appDelegate?.persistentContainer
+        guard let viewContext = container?.viewContext else {return}
+        
+        let request: NSFetchRequest<NSFetchRequestResult> = User.fetchRequest()
+        let deleteRequest = NSBatchDeleteRequest(fetchRequest: request)
+        
+        let result = try? viewContext.execute(deleteRequest)
     }
 
     func fetchData(){
@@ -53,19 +108,25 @@ class LoginSignupViewController: UIViewController {
         print("Count \(result?.count)")
     }
     
-    func fetchOneData(with name: String, and password: String){
+    func fetchUser(login: String, password: String) -> User? {
         
         let appDelegate = UIApplication.shared.delegate as? AppDelegate
         let container = appDelegate?.persistentContainer
-        guard let viewContext = container?.viewContext else {return}
+        guard let viewContext = container?.viewContext else {return nil}
         
         let request: NSFetchRequest<User> = User.fetchRequest()
         
-        request.predicate = NSPredicate(format: "name = %@ AND password = %@", name, password)
+        request.predicate = NSPredicate(format: "name = %@ AND password = %@", login, password)
         let result = try? viewContext.fetch(request)
         
-//        print(result?.first?.songs)
+        if result?.count ?? 0 > 0 {
+            let loggedInModel = result?.first
+//            let loggedInModel = UserModel(login: "", password: "", currentSpent: 1.0, spendToReward: 1.0)
+            return loggedInModel
+        }
+        
 //        print("Count \(result?.count)")
+        return nil
     }
 }
 
